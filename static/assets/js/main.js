f076fef60edbba39e940d283b2e6ebba1d71c83d
Sky471012 <sharma.aakash1012@gmail.com>
2025-03-15 23:23:21 +0530

    Updated navbar

--- static/assets/js/main.js
+++ static/assets/js/main.js
@@ -19,10 +19,10 @@
 
     if (window.pageYOffset > sticky) {
       header_navbar.classList.add("sticky");
-      logo.src = "assets/images/logo/logo-2.svg";
+      logo.src = "./static/assets/images/logo/postify-logo-03-01.png";
     } else {
       header_navbar.classList.remove("sticky");
-      logo.src = "assets/images/logo/logo.svg";
+      logo.src = "./static/assets/images/logo/postify-logo-03-01.png";
     }
 
     // show or hide the back-top-top button
